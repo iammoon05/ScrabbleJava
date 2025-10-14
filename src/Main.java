1fb29c43fdbefebb1ace9b82afa224117d9f5df2
Abdullah Al Mamun <abdullahrens@gmail.com>
2025-10-14 21:58:53 +0200

    Can play game from user input now, although main method only lets user play single player

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,55 @@
-void main() {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-    Scrabble PlayScrabble = new Scrabble();
+public static void main(String[] args) throws IOException {
+    System.out.println("To exit game type: /exit/");
+    System.out.println("Tell me if you want to play Single or MultiPlayer: ");
+    System.out.println("Enter 1 for single and 2 for multiplayer...");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String userInput = null;
 
-    Map<Character, Integer> distMap = PlayScrabble.getDistributionMap();
-    Map<Character, Integer> scoreMap = PlayScrabble.getScoreMap();
+    boolean Valid_Num_of_Player = false;
+    boolean EXIT_GAME = false;
+    int Num_Of_Player = 0;
 
-    char[] distributedPlayerRack = PlayScrabble.createDistributedPlayerRack(7, distMap);
+    // Forces user to choose between single or multi player
+    do {
+        try {
+            userInput = reader.readLine();
+            if (Objects.equals(userInput, "/exit/")) {
+                EXIT_GAME = true;
+                break;
+            }
+            Num_Of_Player = Integer.parseInt(userInput);
+            Valid_Num_of_Player = true;
+        } catch (NumberFormatException e) {
+            System.out.print("Please enter 1 for single and 2 for multiplayer");
+        }
+    } while (!Valid_Num_of_Player && !EXIT_GAME);
 
-    IO.println(Arrays.toString(distributedPlayerRack));
+    Game.PLAYER_MODE Game_Type = Num_Of_Player == 1 ? Game.PLAYER_MODE.Single : Game.PLAYER_MODE.Single;
 
-    ArrayList<String> result = PlayScrabble.findValidWords(distributedPlayerRack, Scrabble.WordDictionary);
-    IO.println(result);
+    Game NewGame = new Game("Abdullah", "None", Game_Type, Game.RACK_TYPE.SingleRack);
 
-    IO.println(PlayScrabble.longestValidWord(result));
-    IO.println(PlayScrabble.highestScoringWord(result, scoreMap));
-    IO.println(PlayScrabble.highestScoringWordWithTripleLetter(result, scoreMap));
+    if (EXIT_GAME) return;
+
+    System.out.print("Player Rack is: ");
+    System.out.println(Arrays.toString(NewGame.Player_1.getPlayerRack()));
+
+    do {
+        try {
+            userInput = reader.readLine();
+            if (Objects.equals(userInput, "/exit/")) {
+                EXIT_GAME = true;
+                break;
+            }
+            NewGame.Play_Game(userInput, NewGame.Player_1);
+
+        } catch (IOException e) {
+            System.err.println("Error reading input: " + e.getMessage());
+        }
+    } while (!EXIT_GAME && NewGame.ShouldGameGoOn()) ;
+
+    reader.close(); // Close the reader when done
 }
